9c8992e2c3da083caba784d6e82b9f29ed40514b
Laminne Tatsuto Yamamoto <gtattoda4@gmail.com>
2020-08-02 22:43:15 +0900

    音を流せるようになった

--- mp3.py
+++ mp3.py
@@ -3,6 +3,7 @@ import asyncio
 import datetime
 import random
 import os
+import time
 client = discord.Client()
 
 @client.event
@@ -10,10 +11,15 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    voice = await.join_voice_channel(client.get_channel())
-    if message.content == ("///rubi start")
-        t = random.choice('music.mp3', 'music2.mp3','music3.mp3')
-        player = await voice.create_ffmpeg_player(t)
-        player.start()
+    global voich
+    # voice = await join_voice_channel(client.get_channel())
+    if message.content == ("///rubi start"):
+        voich = await discord.VoiceChannel.connect(message.author.voice.channel)
+        t = random.choice(('music.mp3','music2.mp3','music3.mp3'))
+        #print(t)
+        voich.play(discord.FFmpegPCMAudio(t))
+        sleep_time = 62
+        await asyncio.sleep(sleep_time)
+        await voich.disconnect()
 
-client.run(os.environ['RUBITYAN_T'])
+client.run("")
